252009cc706f2dfb3a3f4b8cbe2cd0869a66edc6
Conor O'Shea <s2683460@ed.ac.uk>
2026-07-08 11:13:57 +0100

    feat: added parts of skeleton for clip interval extraction

--- video/__init__.py
+++ video/__init__.py
@@ -1,3 +1,3 @@
-from video.convert_input_to_metadata import convert_input_to_metadata
+from video.convert_input_to_metadata import compute_initial_clip_windows
 
-__all__ = ["convert_input_to_metadata"]
+__all__ = ["compute_initial_clip_windows"]
---
+++ video/convert_input_to_metadata.py
@@ -0,0 +1,7 @@
+# should probs use ffmpeg-python to get the video durations, etc.
+
+def compute_initial_clip_windows():
+
+
+
+def get_video_duration()
